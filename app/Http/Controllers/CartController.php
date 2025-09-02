3e44c05f6a8dd85ba6b23837f65a6da4ab2168f7
yojr23 <juniorrincon1992@gmail.com>
2025-09-02 15:27:57 -0500

    creacion de la funcionalidad de carrito

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    /**
+     * Mostrar el carrito
+     */
+    public function index()
+    {
+        $cart = session()->get('cart', []); // si no existe carrito, devuelve []
+        return view('cart', compact('cart'));
+    }
+
+    /**
+     * Agregar producto al carrito
+     */
+    public function add(Request $request)
+    {
+        $cart = session()->get('cart', []);
+
+        $id = $request->id;
+        $name = $request->name;
+        $price = $request->price;
+        $quantity = $request->quantity ?? 1;
+
+        // Si ya existe el producto, sumamos cantidad
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity'] += $quantity;
+        } else {
+            $cart[$id] = [
+                "name" => $name,
+                "price" => $price,
+                "quantity" => $quantity
+            ];
+        }
+
+        session()->put('cart', $cart);
+
+        return redirect()->route('cart.index')->with('success', 'Producto agregado al carrito');
+    }
+
+    /**
+     * Actualizar cantidad de un producto
+     */
+    public function update(Request $request, $id)
+    {
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity'] = $request->quantity;
+            session()->put('cart', $cart);
+        }
+
+        return redirect()->route('cart.index')->with('success', 'Carrito actualizado');
+    }
+
+    /**
+     * Eliminar producto del carrito
+     */
+    public function remove($id)
+    {
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$id])) {
+            unset($cart[$id]);
+            session()->put('cart', $cart);
+        }
+
+        return redirect()->route('cart.index')->with('success', 'Producto eliminado del carrito');
+    }
+
+    /**
+     * Vaciar el carrito completo
+     */
+    public function clear()
+    {
+        session()->forget('cart');
+        return redirect()->route('cart.index')->with('success', 'Carrito vacío');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
     return view('landing');
@@ -9,3 +10,11 @@ Route::get('/', function () {
 Route::get('/producto-a', function () {
     return view('productoA'); // tu página de detalle
 })->name('producto.a');
+
+
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+Route::post('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
+Route::delete('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
+Route::delete('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
